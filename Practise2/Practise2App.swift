6887e186889574780c31d025c4f7a88fb41327ec
Manvendu Pathak <manvendupathak2000@gmail.com>
2024-03-01 13:03:46 +0530

    Initial Commit

---
+++ Practise2/Practise2App.swift
@@ -0,0 +1,17 @@
+//
+//  Practise2App.swift
+//  Practise2
+//
+//  Created by Manvendu Pathak on 01/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Practise2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
